1cb26d5a14c92dc5dc6cba4fed3f9c3cf1ba79b1
isaac-svg <isaacsakyi009@gmail.com>
2023-03-12 22:53:29 +0000

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import "./style.css";
 import "./range.css";
-import { render } from "./utils/render";
 import { App } from "./app";
 const Main = document.querySelector<HTMLDivElement>("#app");
 const app = App();
